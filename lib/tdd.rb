aeebd8e7fef4602ea6d206a619daf73e44f92459
Albert Dedushi <adedushi@hotmail.com>
2023-11-10 14:59:23 -0500

    Create #two_sum spec

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -2,3 +2,7 @@ def my_uniq(arr)
     hash = arr.tally
     new_arr = hash.keys 
 end
+
+def two_sum(arr)
+    
+end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -8,6 +8,17 @@ describe "Array#my_uniq" do
 
     it "returns a new array" do
         expect(my_uniq([1,2,1,3,3])).not_to be ([1,2,3]) 
-
     end
 end
+
+describe "Array#two_sum" do
+    it "return pair of positions where the elements sum to zero" do
+        expect(two_sum([-1, 0, 2, -2, 1])).to eq ([[0, 4], [2, 3]])
+    end
+
+    it "return sorted array with smaller index before bigger index" do
+        expect(two_sum([-1, 0, 2, -2, 1])).not_to eq ([[4, 0], [3, 2]])
+    end
+
+end
+
