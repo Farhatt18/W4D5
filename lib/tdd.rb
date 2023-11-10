bb37c6913d27c3bebad2650bfccc318269a47b4b
Albert Dedushi <adedushi@hotmail.com>
2023-11-10 15:38:22 -0500

    Add to specs for two_sum

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -4,5 +4,14 @@ def my_uniq(arr)
 end
 
 def two_sum(arr)
-    
+    pairs = []
+    arr.each_with_index do |num1, idx1|
+        arr.each_with_index do |num2, idx2|
+            if idx2 > idx1 && num1 + num2 == 0
+                pairs << [idx1, idx2]
+            end
+        end
+    end
+
+    pairs
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -2,12 +2,16 @@ require "rspec"
 require_relative "../lib/tdd.rb"
 
 describe "Array#my_uniq" do 
+    it "does not use built-in Array#uniq" do 
+        expect(my_uniq([1,2,1,3,3])).to eq([1,2,3])   
+    end
+
     it "returns array with duplicates removed" do 
         expect(my_uniq([1,2,1,3,3])).to eq([1,2,3])   
     end
 
     it "returns a new array" do
-        expect(my_uniq([1,2,1,3,3])).not_to be ([1,2,3]) 
+        expect(my_uniq([1,2,1,3,3])).not_to be([1,2,3]) 
     end
 end
 
@@ -16,9 +20,25 @@ describe "Array#two_sum" do
         expect(two_sum([-1, 0, 2, -2, 1])).to eq ([[0, 4], [2, 3]])
     end
 
-    it "return sorted array with smaller index before bigger index" do
-        expect(two_sum([-1, 0, 2, -2, 1])).not_to eq ([[4, 0], [3, 2]])
+    it "find multiple pairs" do
+        expect(two_sum([-1, 67, -67, 0, 1])).to eq ([[0, 4], [1, 2]])
     end
+
+    it "find pairs with same element" do
+        expect(two_sum([6, -6, -6])).to eq ([[0, 1], [0, 2]])
+    end
+
+    it "return empty array if no pair is found" do
+        expect(two_sum([6, -5, -4])).to eq ([])
+    end
+
+    it "return array with sum of real zeros" do
+        expect(two_sum([0, 93434343, 9, 0])).to eq ([[0, 3]])
+    end
+
+    # it "return sorted array with smaller index before bigger index" do
+    #     expect(two_sum([-1, 0, 2, -2, 1])).not_to eq ([[4, 0], [3, 2]])
+    # end
 
 end
 
